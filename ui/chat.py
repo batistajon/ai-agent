482e633f242ec4d90032bfc9d307107d73bf41c5
Jonathas Batista <jonathasab@ciandt.com>
2024-06-10 16:13:00 -0300

    Ajuste de uploads

--- ui/chat.py
+++ ui/chat.py
@@ -5,18 +5,30 @@ import time
 import json
 import requests
 
+
 st.set_page_config(
     page_title="CAQO - Assistente IA",
     page_icon="🤖",
-    layout="centered",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    layout="centered"
 )
 st.title("🤖 Assistente CAQO")
+
+st.sidebar.header("Customize seu assistente")
+llm_choice = st.sidebar.selectbox("Escolha a LLM", {"Premium", "Free"})
+
+if llm_choice == "Premium":
+    llm = "openai"
+else:
+    llm = "ollama"
+uploaded_files = st.sidebar.file_uploader("Suba um PDF para fazer perguntas", accept_multiple_files=True)
+
+def embed_files():
+    for uploaded_file in uploaded_files:
+        st.markdown("file uploaded")
+
+if uploaded_files:
+    st.sidebar.button(label="Treinar IA", on_click=embed_files)
+
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -33,6 +45,7 @@ if prompt := st.chat_input("Em que posso te ajudar?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
+
         response_prompt = """
             Use the following pieces of context to answer the question at the end.
             If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -47,7 +60,7 @@ if prompt := st.chat_input("Em que posso te ajudar?"):
 
         params = {
             "token": "lskdjfhlasdhflaskjdhflaksdjhlfkjasdlkfjahlsdj",
-            "llm": "openai",
+            "llm": llm,
             "client": "caqo",
             "category": "recursos",
             "subject": "deploy",
@@ -60,8 +73,6 @@ if prompt := st.chat_input("Em que posso te ajudar?"):
             "Content-Type": "application/json"
         }
 
-        print("Request URL:", url)
-        print("Headers:", headers)
         print("Parameters:", json.dumps(params, indent=2))
         json_data = json.dumps(params)
         answer=""
@@ -79,7 +90,7 @@ if prompt := st.chat_input("Em que posso te ajudar?"):
         response_md = st.empty()
         for chunk in answer:
             response += chunk
-            time.sleep(0.05)
+            time.sleep(0.01)
             response_md.markdown(response)
 
     st.session_state.messages.append({"role": "assistant", "content": response})
