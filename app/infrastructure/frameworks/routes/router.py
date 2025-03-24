45188be63495e1a014e8f100aac1be8b8c7f0634
Jon Batista <batista.jonathas@gmail.com>
2025-03-24 08:49:06 -0300

    adjust endpoints

--- app/infrastructure/frameworks/routes/router.py
+++ app/infrastructure/frameworks/routes/router.py
@@ -17,32 +17,17 @@ def index() -> JSONResponse:
     return response
 
 
-@router.post("/training/pdf")
+@router.post("/agent/train/pdf")
 def training():
     response = JSONResponse({
         "error": False,
-        "status": "live",
         "message": "training by PDF"
     }, status_code=201)
 
     return response
 
 
-@router.post("/query/llm")
-def askLLM():
-    llm_factory = LLMFactory()
-    llm_strategy = llm_factory.get_llm_strategy(llm_key="deepseek")
-    llm = llm_strategy.get_instance()
-
-    response = JSONResponse({
-        "error": False,
-        "message": llm
-    }, status_code=200)
-
-    return response
-
-
-@router.post("/llms/rag")
+@router.post("/agent/ask")
 def askRAG():
     llm_factory = LLMFactory()
     llm_strategy = llm_factory.get_llm_strategy(llm_key="deepseek")
